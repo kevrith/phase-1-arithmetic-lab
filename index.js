8bb6fc55b05e321d4b278213ac8f4045a5d79a9a
KELVIN <kevrith@gmail.com>
2025-09-10 08:06:19 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1*num2
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) +1;
+console.log(random)
+
+const num3 = 24;
+const num4 = 5;
+let mod = num3%num4;
+console.log(mod);
+
+let a = [1,15,7,19,5,8,6,11,14,17,20,12,13,18]
+const max = Math.max(...a);
+console.log(max)
